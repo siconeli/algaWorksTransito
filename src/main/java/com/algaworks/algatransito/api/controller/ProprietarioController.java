749ee47e938e304a8965bc2bf4167716d8cc66b0
siconeli <henrique.siconeli.f@hotmail.com>
2024-08-04 13:27:32 -0300

    up

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,30 +1,26 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
+    private ProprietarioRepository proprietarioRepository;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var prop1 = new Proprietario();
-        prop1.setId(1L);
-        prop1.setNome("Henrique Siconeli");
-        prop1.setEmail("henrique@gmail.com");
-        prop1.setTelefone("67 99964-9982");
-
-        var prop2 = new Proprietario();
-        prop2.setId(2L);
-        prop2.setNome("Benício Siconeli");
-        prop2.setEmail("benicio@gmail.com");
-        prop2.setTelefone("67 99131-0698");
-
-        return Arrays.asList(prop1, prop2);
-
+//        return proprietarioRepository.findByNomeContaining("Cristina");
+        return proprietarioRepository.findAll();
     }
 }
