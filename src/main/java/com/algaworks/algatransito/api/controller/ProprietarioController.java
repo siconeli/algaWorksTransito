7857cb39aea507946d3ba498a9181a011b05a12d
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-08-05 09:48:29 -0400

    método atualizar no controller

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -33,4 +33,16 @@ public class ProprietarioController {
     public Proprietario adicionar (@RequestBody Proprietario proprietario)  {
         return proprietarioRepository.save(proprietario);
     }
+
+    @PutMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> atualizar (@PathVariable Long proprietarioId, @RequestBody Proprietario proprietario) {
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietario.setId(proprietarioId);
+        Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
+        return ResponseEntity.ok(proprietarioAtualizado);
+
+    }
 }
