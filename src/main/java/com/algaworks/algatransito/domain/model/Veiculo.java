3d137a3f6809072396e6da34e0e03de736b55ff4
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-08-07 17:02:07 -0400

    criação da entidade veiculo

---
+++ src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -0,0 +1,36 @@
+package com.algaworks.algatransito.domain.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Veiculo {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Proprietario proprietario;
+
+    private String marca;
+
+    private String modelo;
+
+    private String placa;
+
+    @Enumerated(EnumType.STRING)
+    private StatusVeiculo status;
+
+    private LocalDateTime dataCadastro;
+
+    private LocalDateTime dataApreensao;
+}
