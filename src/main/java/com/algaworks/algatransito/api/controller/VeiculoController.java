e10e93149fb69e2551c39ab2884424b4386a739c
siconeli <henrique.siconeli.f@hotmail.com>
2024-08-08 00:03:08 -0300

    up

--- src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -1,13 +1,12 @@
 package com.algaworks.algatransito.api.controller;
 
+import com.algaworks.algatransito.domain.exception.VeiculoException;
 import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.repository.VeiculoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +25,18 @@ public class VeiculoController {
     @GetMapping("/{veiculoId}")
     public ResponseEntity<Veiculo> buscar (@PathVariable Long veiculoId) {
         return veiculoRepository.findById(veiculoId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Veiculo adicionar (@RequestBody Veiculo veiculo) {
+        boolean placaEmUso = veiculoRepository.findByPlaca(veiculo.getPlaca()).filter(v -> !v.equals(veiculo)).isPresent();
+
+        if (placaEmUso) {
+            throw new VeiculoException("Já existe um veículo cadastrado com esta placa");
+        }
+
+        return veiculoRepository.save(veiculo);
     }
 
 
---
+++ src/main/java/com/algaworks/algatransito/domain/exception/VeiculoException.java
@@ -0,0 +1,6 @@
+package com.algaworks.algatransito.domain.exception;
+
+public class VeiculoException extends RuntimeException{
+
+    public VeiculoException (String message) { super(message); }
+}
