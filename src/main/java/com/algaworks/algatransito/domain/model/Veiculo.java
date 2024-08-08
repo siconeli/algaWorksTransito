0ce3fb4aa6962033f0ff983c4dfd0fe6c8db17fe
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-08-08 11:34:25 -0400

    adicionando apenas leitura nos atributos em que os dados são preenchidos pela classe de service, não deixando o usuário inserir dados no atributo de forma maliciosa

--- src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -27,10 +28,13 @@ public class Veiculo {
 
     private String placa;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusVeiculo status;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataCadastro;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataApreensao;
 }
